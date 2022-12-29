6658c79e13121511ae1886e3c5ce2e333a44ff1d
vinicius.sibin <vinisibim@gmail.com>
2022-12-29 14:20:21 -0300

    versão 1 do front finalizado

---
+++ paginas/lojaDeCursos.php
@@ -0,0 +1,46 @@
+<!-- Page-header start -->
+<div class="page-header card">
+<div class="row align-items-end">
+        <div class="col-lg-8">
+            <div class="page-header-title">
+                <i class="icofont icofont icofont icofont-bag bg-c-pink"></i>
+                <div class="d-inline">
+                    <h4>Loja de Cursos</h4>
+                    <span>Aqui estão todos os cursos disponíveis em nosso sistema!</span>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="page-header-breadcrumb">
+                <ul class="breadcrumb-title">
+                    <li class="breadcrumb-item">
+                        <a href="index.php">
+                            <i class="icofont icofont-home"></i>
+                        </a>
+                    </li>
+                    <li class="breadcrumb-item"><a href="#!">Loja de Cursos</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Page-header end -->
+
+<div class="page-body">
+    <div class="row">
+        <div class="col-sm-3">
+            <div class="card">
+                <div class="card-block">
+                    <img src="assets/images/bg.jpg" alt="" class="img-fluid">
+                    <p>
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                        enim ad minim veniam, quis nostrud ...
+                    </p>
+                    <button type="button" class="btn btn-primary btn-md btn-block waves-effect text-center m-b-20">Valor R$29,90</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- paginas/paginaInicial.php
+++ paginas/paginaInicial.php
@@ -3,9 +3,9 @@
     <div class="row align-items-end">
         <div class="col-lg-8">
             <div class="page-header-title">
-                <i class="icofont icofont icofont icofont-file-document bg-c-pink"></i>
+                <i class="icofont icofont icofont icofont-home bg-primary"></i>
                 <div class="d-inline">
-                    <h4>Sample Page</h4>
+                    <h4>Página inicial</h4>
                     <span>lorem ipsum dolor sit amet, consectetur adipisicing elit</span>
                 </div>
             </div>
@@ -14,13 +14,11 @@
             <div class="page-header-breadcrumb">
                 <ul class="breadcrumb-title">
                     <li class="breadcrumb-item">
-                        <a href="index.html">
+                        <a href="index.php">
                             <i class="icofont icofont-home"></i>
                         </a>
                     </li>
-                    <li class="breadcrumb-item"><a href="#!">Pages</a>
-                    </li>
-                    <li class="breadcrumb-item"><a href="#!">Sample page</a>
+                    <li class="breadcrumb-item"><a href="#!">Página inicial</a>
                     </li>
                 </ul>
             </div>
@@ -34,27 +32,12 @@
         <div class="col-sm-12">
             <div class="card">
                 <div class="card-header">
-                    <h5>Hello card</h5>
-                    <span>lorem ipsum dolor sit amet, consectetur adipisicing elit</span>
-                    <div class="card-header-right">
-                        <ul class="list-unstyled card-option" style="width: 35px;">
-                            <li class=""><i class="icofont icofont-simple-left"></i></li>
-                            <li><i class="icofont icofont-maximize full-card"></i></li>
-                            <li><i class="icofont icofont-minus minimize-card"></i></li>
-                            <li><i class="icofont icofont-refresh reload-card"></i></li>
-                            <li><i class="icofont icofont-error close-card"></i></li>
-                        </ul>
-                    </div>
+                    <h5>Página inicial</h5>
+                    <span>Seja bem vindo à Vtec - EAD</span>
                 </div>
                 <div class="card-block">
                     <p>
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                        enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                        in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                        nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                        sunt in culpa qui officia deserunt mollit anim id est laborum."
+                        Na Vtec - EAD nós ensinamos vários cursos muito interessantes para você de forma acessível, prática e simples.
                     </p>
                 </div>
             </div>
