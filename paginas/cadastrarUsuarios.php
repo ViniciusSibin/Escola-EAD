28779a1e00ba88ada4a33fe9c9ec4d9568e3659d
vinicius.sibin <vinisibim@gmail.com>
2023-01-02 16:19:15 -0300

    Finalizado fomulario de cadastro de usuários

--- paginas/cadastrarUsuarios.php
+++ paginas/cadastrarUsuarios.php
@@ -1,59 +1,66 @@
-<div class="auth-body mr-auto ml-auto col-md-6">
-    <form class="md-float-material">
+<div class="auth-body mr-auto ml-auto col-md-7">
+    <form class="md-float-material" method="POST">
         <div class="auth-box">
             <div class="col-md-12">
-                <h3 class="text-left txt-primary">Cadastrar novo curso</h3>
-                <p class="m-t-30 text-left" style="color: black;">Informe os dados abaixo para cadastrar o curso!</p>
+                <h3 class="text-left txt-primary">Cadastrar novo usuário</h3>
+                <p class="m-t-30 text-left" style="color: black;">Informe os dados abaixo para cadastrar o usuário!</p>
             </div>
             <hr/>
             <div class="form-group row">
-                <label class="col-sm-2 col-form-label">Titulo: </label>
+                <label class="col-sm-2 col-form-label">Nome:</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" placeholder="Titulo do curso">
+                    <input type="text" class="form-control" placeholder="Nome do usuário" name="nome">
                 </div>
             </div>
             <div class="form-group row">
-                <label class="col-sm-2 col-form-label">Professor: </label>
+                <label class="col-sm-2 col-form-label">E-mail:</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" placeholder="Nome do professor">
+                    <input type="text" class="form-control" placeholder="Ex: exemplo@exemplo.com" name="email">
                 </div>
             </div>
             <div class="form-group row">
-                <label class="col-sm-2 col-form-label">Carga: </label>
+                <label class="col-sm-2 col-form-label">Telefone:</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" placeholder="Carga Horária">
+                    <input type="text" class="form-control" placeholder="Ex:(44)98765-4321" name="telefone">
                 </div>
             </div>
             <div class="form-group row">
-                <label class="col-sm-2 col-form-label">Valor: </label>
+                <label class="col-sm-2 col-form-label">Data Nascimento:</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" placeholder="Valor do curso">
+                    <input type="text" class="form-control" placeholder="Ex:00/00/000" name="nascimento">
                 </div>
             </div>
             <div class="form-group row">
-                <label class="col-sm-2 col-form-label">Imagem: </label>
+                <label class="col-sm-2 col-form-label">Senha:</label>
                 <div class="col-sm-10">
-                    <input type="file" class="form-control" placeholder="Carga Horária">
+                    <input type="password" class="form-control" placeholder="Senha" name="senha">
                 </div>
             </div>
             <div class="form-group row">
-                <label class="col-sm-2 col-form-label">Descrição: </label>
+                <label class="col-sm-2 col-form-label">Confirmar Senha:</label>
                 <div class="col-sm-10">
-                    <textarea rows="5" col="5" class="form-control" placeholder="Descrição curta max: 300 caracteres"></textarea>
+                    <input type="password" class="form-control" placeholder="Confirmar Senha" name="confirmarSenha">
                 </div>
             </div>
             <div class="form-group row">
-                <label class="col-sm-2 col-form-label">Conteudo: </label>
+                <label class="col-sm-2 col-form-label">Administrador? </label>
                 <div class="col-sm-10">
-                    <textarea rows="5" col="5" class="form-control" placeholder="Conteúdo do curso, max 1000 caracteres"></textarea>
+                    <div><input type="radio" name="admin" value="1"> SIM</div>
+                    <div><input type="radio" name="admin" value="0"> NÃO</div>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label">Foto de Perfil: </label>
+                <div class="col-sm-10">
+                    <input type="file" class="" name="fotoPerfil">
                 </div>
             </div>
             <div class="row m-t-20">
                 <div class="col-md-6">
-                    <button type="button" class="btn btn-danger btn-md btn-block waves-effect text-center m-b-20"><a href="login.html" style="text-decoration: none; color: #fff;">Cancelar</a></button>
+                    <a href="index.php?pagina=gerenciarUsuarios" class="btn btn-danger btn-md btn-block waves-effect text-center m-b-20">Cancelar</a>
                 </div>
                 <div class="col-md-6">
-                    <button type="button" class="btn btn-primary btn-md btn-block waves-effect text-center m-b-20">Enviar</button>
+                    <button type="submit" class="btn btn-primary btn-md btn-block waves-effect text-center m-b-20">Enviar</button>
                 </div>
             </div>
         </div>
