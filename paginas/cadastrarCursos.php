541067a23acc155063c3522ff299f73276b4e10c
vinicius.sibin <vinisibim@gmail.com>
2023-01-03 10:47:05 -0300

    Arrumado inputs de carga e valor, e feito a verificação dos campos titulo e professor

--- paginas/cadastrarCursos.php
+++ paginas/cadastrarCursos.php
@@ -2,6 +2,7 @@
 
 if(isset($_POST['enviar'])){
     include('lib/conexao.php');
+    include('lib/funcoes.php');
 
     $erro = false;
 
@@ -16,10 +17,12 @@ if(isset($_POST['enviar'])){
     $descricao = $_POST['descricao'];
     $conteudo = $_POST['conteudo'];
 
+    $erro = verificaUsuario($titulo, 5, 100, 'titulo');
+    $erro = verificaUsuario($professor, 5, 50, "professor");
+
     if(!$erro){
 
-    }else{
-        }
+    }
 }
 
 ?>
@@ -34,25 +37,25 @@ if(isset($_POST['enviar'])){
             <div class="form-group row">
                 <label class="col-sm-2 col-form-label">Titulo:</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" placeholder="Titulo do curso" name="titulo">
+                    <input type="text" class="form-control" placeholder="Titulo do curso" name="titulo" value="<?php if(isset($_POST['titulo'])) echo $_POST['titulo']; ?>">
                 </div>
             </div>
             <div class="form-group row">
                 <label class="col-sm-2 col-form-label">Professor:</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" placeholder="Nome do professor" name="professor">
+                    <input type="text" class="form-control" placeholder="Nome do professor" name="professor" value="<?php if(isset($_POST['professor'])) echo $_POST['professor']; ?>">
                 </div>
             </div>
             <div class="form-group row">
                 <label class="col-sm-2 col-form-label">Carga:</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" placeholder="Carga Horária" name="carga">
+                    <input type="number" class="form-control" min="0.00" max="10000.00" step="1" placeholder="Carga horaria do curso" name="carga" value="<?php if(isset($_POST['carga'])) echo $_POST['carga']; ?>">
                 </div>
             </div>
             <div class="form-group row">
                 <label class="col-sm-2 col-form-label">Valor:</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" placeholder="Valor do curso" name="valor">
+                    <input type="number" class="form-control" min="0.00" max="10000.00" step="0.01" placeholder="valor do curso"  name="valor" value="<?php if(isset($_POST['valor'])) echo $_POST['valor']; ?>">
                 </div>
             </div>
             <div class="form-group row">
@@ -64,26 +67,34 @@ if(isset($_POST['enviar'])){
             <div class="form-group row">
                 <label class="col-sm-2 col-form-label">Descrição:</label>
                 <div class="col-sm-10">
-                    <textarea rows="5" col="5" class="form-control" placeholder="Descrição curta max: 300 caracteres" name="descricao"></textarea>
+                    <textarea rows="5" col="5" class="form-control" placeholder="Descrição curta max: 300 caracteres" name="descricao" value="<?php if(isset($_POST['descricao'])) echo $_POST['descricao']; ?>"></textarea>
                 </div>
             </div>
             <div class="form-group row">
                 <label class="col-sm-2 col-form-label">Conteudo:</label>
                 <div class="col-sm-10">
-                    <textarea rows="5" col="5" class="form-control" placeholder="Conteúdo do curso, max 1000 caracteres" name="conteudo"></textarea>
+                    <textarea rows="5" col="5" class="form-control" min="10" max="1000" placeholder="Conteúdo do curso, max 1000 caracteres" name="conteudo" value="<?php if(isset($_POST['conteudo'])) echo $_POST['conteudo']; ?>"></textarea>
                 </div>
             </div>
-            <div class="form-group row">
-                <label class="col-sm-2 col-form-label"></label>
-                <p class="col-sm-10 col-form-label text-danger">ERRO: teste</p>
-            </div>
-            
+            <?php if(count($_POST) > 0 && $erro){ ?>
+                <div class="form-group row">
+                    <label class="col-sm-2 col-form-label"></label>
+                    <p class="col-sm-10 col-form-label text-danger">ERRO: <?php echo $erro ?></p>
+                </div>
+            <?php } else if (count($_POST) > 0) {?>
+                <div class="form-group row">
+                    <label class="col-sm-2 col-form-label"></label>
+                    <p class="col-sm-10 col-form-label text-success">Curso cadastrado com sucesso!</p>
+                </div>
+            <?php 
+             unset($_POST);
+            }?>
             <div class="row m-t-20">
                 <div class="col-md-6">
                     <a href="login.html" class="btn hor-grd btn-grd-danger btn-md btn-block waves-effect text-center m-b-20">Cancelar</a>
                 </div>
                 <div class="col-md-6">
-                    <button type="submit" name='enviar' value='1' class="btn hor-grd btn-grd-primary btn-md btn-block waves-effect text-center m-b-20"><i class="ti-save"></i> Enviar</button>
+                    <button type="submit" name='enviar' value='1' class="btn hor-grd btn-grd-primary btn-md btn-block waves-effect text-center m-b-20"><i class="ti-save"></i>Enviar</button>
                 </div>
             </div>
         </div>
