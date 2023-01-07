8066407ddb605bc351c1de697c0c7d31c5675bcb
vinicius.sibin <vinisibim@gmail.com>
2023-01-07 10:26:11 -0300

    Feito página para deletar cursos

---
+++ paginas/curso-deletar.php
@@ -0,0 +1,39 @@
+<?php 
+    include('lib/conexao.php');
+
+    $idCurso = $_GET['idCurso'];
+    $sqlCurso = "SELECT * FROM cursos WHERE id = '$idCurso'";
+    $sqlCursoQuery = $mysqli->query($sqlCurso);
+    $curso = $sqlCursoQuery->fetch_assoc();
+    
+    var_dump($curso);
+    if(isset($_POST['confirma'])){
+        $sqlDeletaCurso = "DELETE FROM cursos WHERE id = '$idCurso'";
+        $sqlDeletaCursoQuery = $mysqli->query($sqlDeletaCurso);
+        if(!$sqlDeletaCursoQuery){
+            $erro = "Falha ao deletar o curso do banco de dados";
+        } else {
+            die("<script>location.href=\"index.php?pagina=curso-gerenciar\"</script>");
+        }
+    }
+?>
+
+<div class="auth-body mr-auto ml-auto col-md-5">
+    <form class="md-float-material" method="POST">
+        <div class="auth-box">
+            <div class="col-md-12">
+                <h2 class="text-left txt-primary">Deletar o curso <b><?php echo $curso['titulo'] ?></b></h2>
+                <p class="m-t-30 text-left" style="color: black;">Tem certeza que deseja deletar o curso ?</p>
+            </div>
+            <hr/>
+            <div class="row m-t-20">
+                <div class="col-md-6">
+                    <button type="submit" name="confirma" value="1" class="btn hor-grd btn-grd-success btn-md btn-block waves-effect text-center">Deletar</a>
+                </div>
+                <div class="col-md-6">
+                    <a class="btn hor-grd btn-grd-danger btn-md btn-block waves-effect text-center" href="index.php?pagina=curso-gerenciar" style="text-decoration: none; color: #fff;">Cancelar</a>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
