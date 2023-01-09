bcf35beb65348dd59430ccc94a753b2ee86339ca
Vinicius Sibin <vinisibim@gmail.com>
2023-01-09 16:20:34 -0300

    Arrumado função de proteção

--- lib/protect.php
+++ lib/protect.php
@@ -1,15 +1,16 @@
 <?php
 
 function protect($admin){
-    if(isset($_SESSION)){
+
+    if(!isset($_SESSION)){
         session_start();
     }
-
+    
     if(!isset($_SESSION['usuario'])){
         header("Location: login.php");
     }
 
-    if(!isset($_SESSION['admin']) || ($admin == 1 && $_SESSION['admin'] != 1)){
+    if($admin == 1 && $_SESSION['admin'] != 1){
         header("Location: login.php");
     }
 }
