3c7c1ce8c2225b35cf5208de2a44bfa8453bfbdd
Vinicius Sibin <vinisibim@gmail.com>
2023-01-09 16:58:54 -0300

    Criado uma condição para funcionar a function protect

--- lib/protect.php
+++ lib/protect.php
@@ -1,16 +1,17 @@
 <?php
+if(!function_exists("protect")){
+    function protect($admin){
 
-function protect($admin){
+        if(!isset($_SESSION)){
+            session_start();
+        }
+        
+        if(!isset($_SESSION['usuario'])){
+            header("Location: login.php");
+        }
 
-    if(!isset($_SESSION)){
-        session_start();
-    }
-    
-    if(!isset($_SESSION['usuario'])){
-        header("Location: login.php");
-    }
-
-    if($admin == 1 && $_SESSION['admin'] != 1){
-        header("Location: login.php");
+        if($admin == 1 && $_SESSION['admin'] != 1){
+            header("Location: login.php");
+        }
     }
 }
